d66386aa86405e248c9d7cb2b197c5e2b1fca4d7
montag <arslankhannizamani12@gmail.com>
2026-05-22 12:09:28 +0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Social Preview Modifier",
+    "version": "1.0.0",
+    "category": "Website",
+    "depends": ["web"],
+    "data": [
+        "views/modify_login_layout.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
